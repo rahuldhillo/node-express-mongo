7acd8b95148631604b2b568bbbb81c188a58c4d5
rahul <rahuldhillon27@gmail.com>
2021-11-30 08:47:36 +0530

    new changes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,6 +4,7 @@ import { getPosts, createPost } from "../controllers/posts.js";
 const router = express.Router();
 
 router.get("/", getPosts);
+
 router.post("/", createPost);
 
 export default router;
