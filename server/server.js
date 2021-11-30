8648c88f10e5e34e571b4b71fdbdf7aa0bbb615c
rahul <rahuldhillon27@gmail.com>
2021-11-30 07:57:00 +0530

    created react client and node server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+const db = require("./app/models");
 
 var corsOptions = {
   origin: "http://localhost:8081",
@@ -20,6 +21,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the application." });
 });
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
