cb664cb34a9f4e1ca9a6c740bf836759d0f27dcb
rahul <rahuldhillon27@gmail.com>
2021-12-02 06:18:36 +0530

    Before changes - removing mongoose - removing mongodb atlas

--- server/index.js
+++ server/index.js
@@ -8,24 +8,30 @@ import postRoutes from "./routes/posts.js";
 const app = express();
 
 //prefix for posts
-app.use("/posts", postRoutes);
+app.use("/", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://nem:nem123@cluster0.ranyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+// "mongodb+srv://nem:nem123@cluster0.ranyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
-  )
-  .catch((error) => console.log(`${error} did not connect`));
+app.set("port", PORT);
+
+app.listen(PORT, () => {
+  console.log("Listening on port " + app.get("port"));
+});
+
+// mongoose
+//   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() =>
+//     app.listen(PORT, () =>
+//       console.log(`Server Running on Port: http://localhost:${PORT}`)
+//     )
+//   )
+//   .catch((error) => console.log(`${error} did not connect`));
 
 // mongoose.set("useFindAndModify", false);
